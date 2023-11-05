3e930fdf13a2473a31ba62f9a04cda01a4884b1f
Vincent <bsn.vincent@gmail.com>
2023-11-05 14:20:43 +0100

    init livewire datatables

---
+++ app/Http/Livewire/Foo.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Mediconesystems\LivewireDatatables\Http\Livewire\LivewireDatatable;
+
+class Foo extends LivewireDatatable
+{
+    public function builder()
+    {
+        //
+    }
+
+    public function columns()
+    {
+        //
+    }
+}
